9d9da9b18b4c1543216c60995b69858c9f3f89b3
justin <justin61013@gmail.com>
2022-11-22 13:56:32 +0800

    fix load_dict bug

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ import flwr as fl
 import pruning
 from efficient import *
 
-model = EfficientNetB0()
 if __name__ == "__main__":
     while(5):
+        model = EfficientNetB0()
         pr = pruning.pruning(model, 'efficientnet-b0.pth')
         fl.server.start_server(server_address="127.0.0.1:12345", config=fl.server.ServerConfig(num_rounds=5))
         pr.process()
